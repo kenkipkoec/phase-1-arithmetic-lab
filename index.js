473936d6cb769cbb6e8789fd159f173d016e5bed
kenkipkoec <itsmetrokenaki@gmal.com>
2024-03-27 12:17:59 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let multiply
 
-// Write your code here
+let num1 = 2
+
+let num2 = 31
+multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod
+
+let num3 = 20
+
+let num4 = 5
+mod = num3 / num4
+
+let max
+
+const mySet = new Set([10, 5, 20, 2])
+max = Math.max(...mySet)
